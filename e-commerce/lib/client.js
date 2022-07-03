bd6633ca1c66b82ac5e7a31ced06bec37478617c
rishabvivek <rishabv752@gmail.com>
2022-07-03 16:32:16 +0800

    added folder

---
+++ e-commerce/lib/client.js
@@ -0,0 +1,17 @@
+import sanityClient from '@sanity/client';
+
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = sanityClient( {
+    projectId: 'kl3d2cnw',
+    dataset: 'production',
+    apiVersion: '2022-07-01',
+    useCdn: true,
+    token: process.env.SANITY_TOKEN,
+
+
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
